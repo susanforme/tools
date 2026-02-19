ee9d2996b529348948013799b9005f7e030209ff
susanforme <rzc307853639@gmail.com>
2026-02-19 17:08:15 +0800

    feat: 恢复 logLevel 配置以增强调试输出

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 const config = defineConfig({
-  // logLevel: 'warn',
+  logLevel: 'warn',
   plugins: [
     devtools(),
     tsconfigPaths({ projects: ['./tsconfig.json'] }),
